575988d0b01634f82570d78bc683984540c29e1e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-09-28 10:12:09 +0200

    Omit trivial resolvers

--- src/index.js
+++ src/index.js
@@ -25,11 +25,6 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
-  },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
   }
 };
 

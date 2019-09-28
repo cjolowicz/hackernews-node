2e30d11cec93984d71da4583e4227f82333b95af
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-09-28 18:38:19 +0200

    Implement link count in feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,7 @@ function info() {
   return `This is the API of a Hackernews Clone`;
 }
 
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
   const where = args.filter
     ? {
         OR: [
@@ -12,12 +12,24 @@ function feed(parent, args, context, info) {
       }
     : {};
 
-  return context.prisma.links({
+  const links = await context.prisma.links({
     where,
     skip: args.skip,
     first: args.first,
     orderBy: args.orderBy
   });
+
+  const count = await context.prisma
+    .linksConnection({ where })
+    .aggregate()
+    .count();
+
+  return {
+    links,
+    count
+  };
+
+  return links;
 }
 
 module.exports = {

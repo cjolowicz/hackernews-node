474180ab8b846691f97d562a2758911fe4007fcc
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-09-28 12:46:38 +0200

    Remove in-memory implementation

--- src/index.js
+++ src/index.js
@@ -1,59 +1,8 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-
-let idCount = links.length;
-
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => {
-      const found = links.filter(link => link.id === args.id);
-      return found.length ? found[0] : null;
-    }
-  },
-  Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent, args) => {
-      let i = links.length;
-      while (i--) {
-        const link = links[i];
-        if (link.id === args.id) {
-          for ([key, value] of Object.entries(args)) {
-            if (value != null) {
-              link[key] = value;
-            }
-          }
-          return link;
-        }
-      }
-    },
-    deleteLink: (parent, args) => {
-      let i = links.length;
-      while (i--) {
-        const link = links[i];
-        if (link.id === args.id) {
-          links.splice(i, 1);
-          return link;
-        }
-      }
-    }
+    info: () => `This is the API of a Hackernews Clone`
   }
 };
 

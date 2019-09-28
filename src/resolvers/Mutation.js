aaba2bae8c12f79f9958527a896d26869ce85762
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-09-28 14:34:22 +0200

    Implement post mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { APP_SECRET } = require("../utils");
+const { APP_SECRET, getUserId } = require("../utils");
 
 async function signup(parent, args, context, info) {
   const password = await bcrypt.hash(args.password, 10);
@@ -32,7 +32,17 @@ async function login(parent, args, context, info) {
   };
 }
 
+function post(parent, args, context, info) {
+  const userId = getUserId(context);
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: { connect: { id: userId } }
+  });
+}
+
 module.exports = {
   signup,
-  login
+  login,
+  post
 };

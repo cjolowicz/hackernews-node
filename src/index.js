be1e62d7ae1fb6a0bc2762a6a301251c5cc09596
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-09-28 12:58:21 +0200

    Attach Prisma client to GraphQL server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { prisma } = require("./generated/prisma-client");
 const { GraphQLServer } = require("graphql-yoga");
 
 const resolvers = {
@@ -19,7 +20,8 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
